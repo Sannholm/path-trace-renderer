9bfde094045f7c16e646820cd78862fa0b0b84b5
Benjamin Sannholm <benjamin@sannholm.se>
2016-10-08 01:31:21 +0200

    Cornell-ish box and some basic attempts at multi traced lighting

--- src/main/java/benjaminsannholm/util/math/MathUtils.java
+++ src/main/java/benjaminsannholm/util/math/MathUtils.java
@@ -8,29 +8,41 @@ public final class MathUtils
             return start;
         return start + (end - start) * factor;
     }
-
+    
     public static float slerp(float start, float end, float factor)
     {
         float diff = end - start;
-
+        
         while (diff < -180)
             diff += 360;
-
+        
         while (diff >= 180)
             diff -= 360;
-
+        
         return start + diff * factor;
     }
-
+    
     public static boolean isBetween(float value, float min, float max)
     {
         return min <= value && value <= max;
     }
-    
+
     public static boolean isPointInBounds(Vector3 point, Vector3 min, Vector3 max)
     {
         return isBetween(point.getX(), min.getX(), max.getX())
                 && isBetween(point.getY(), min.getY(), max.getY())
                 && isBetween(point.getZ(), min.getZ(), max.getZ());
+    }
+
+    public static int nextPoT(int x)
+    {
+        x--;
+        x |= x >> 1; // handle 2 bit numbers
+        x |= x >> 2; // handle 4 bit numbers
+        x |= x >> 4; // handle 8 bit numbers
+        x |= x >> 8; // handle 16 bit numbers
+        x |= x >> 16; // handle 32 bit numbers
+        x++;
+        return x;
     }
 }
