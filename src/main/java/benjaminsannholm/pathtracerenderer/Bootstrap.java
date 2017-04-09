b4bb0f6edc1261bf08070ff4597a65c79730142c
Benjamin Sannholm <benjamin@sannholm.se>
2017-04-09 12:12:16 +0200

    Make RNG seed consistent between parallel and serial sampling + Minor fixes and cleanup

--- src/main/java/benjaminsannholm/pathtracerenderer/Bootstrap.java
+++ src/main/java/benjaminsannholm/pathtracerenderer/Bootstrap.java
@@ -74,57 +74,57 @@ import benjaminsannholm.util.resource.ResourceLocator;
 public class Bootstrap
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(Bootstrap.class);
-    
-    private static final Random RAND = ThreadLocalRandom.current();
 
+    private static final Random RAND = ThreadLocalRandom.current();
+    
     private static final int WINDOW_WIDTH = 256;
     private static final int WINDOW_HEIGHT = 256;
-
-    private static final int PASSES_PER_FRAME = 10;
     
+    private static final int PASSES_PER_FRAME = 100;
+
     private static final ResourceLocator BASE_RESOURCE_LOCATOR = new PrefixedResourceLocator(
             new ClasspathResourceLocator(), "/");
-    
+
     private final TextureManager textureManager = new TextureManager(
             new PrefixedResourceLocator(BASE_RESOURCE_LOCATOR, "textures/"));
-    
+
     private final ShaderManager shaderManager = new ShaderManager(new FallbackResourceLocator(
             new PrefixedResourceLocator(new FileResourceLocator(), "shaders/"),
             new PrefixedResourceLocator(BASE_RESOURCE_LOCATOR, "shaders/")), 430, false);
-
+    
     private long window;
     private int width;
     private int height;
-
+    
     private double prevLoopTime;
     private double timeElapsed;
-
+    
     private double lastFPSTime;
     private int fpsCounter;
     private int fps;
-
+    
     private double lastFrameTime;
-
+    
     private int numPasses;
     private float sceneTime;
-
-    private Texture2D mainFrameBufferTex;
     
+    private Texture2D mainFrameBufferTex;
+
     private Transform cameraTransform;
     private Matrix4 projectionMatrix;
     private Matrix4 viewMatrix;
     private Vector4 cam00, cam10, cam01, cam11;
-    
+
     private Query qSetup, qCompute, qBarrier, qComposite;
     private long totalFrameTime;
-
+    
     public void run()
     {
         try
         {
             init();
             loop();
-
+            
             glfwFreeCallbacks(window);
             glfwDestroyWindow(window);
         }
@@ -134,27 +134,27 @@ public class Bootstrap
             glfwSetErrorCallback(null).free();
         }
     }
-
+    
     private void init()
     {
         setupGLWindow();
         //GLAPI.setFramebufferSRGB(true);
-
+        
         qSetup = new Query(Type.TIME_ELAPSED);
         qCompute = new Query(Type.TIME_ELAPSED);
         qBarrier = new Query(Type.TIME_ELAPSED);
         qComposite = new Query(Type.TIME_ELAPSED);
-        
+
         setupScene(sceneTime);
     }
-    
+
     private void setupGLWindow()
     {
         GLFWErrorCallback.createPrint(System.err).set();
-        
+
         if (!glfwInit())
             throw new IllegalStateException("Unable to initialize GLFW");
-        
+
         glfwDefaultWindowHints();
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
@@ -163,19 +163,19 @@ public class Bootstrap
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
         glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
         glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
-        
+
         window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Renderer", NULL, NULL);
         if (window == NULL)
             throw new RuntimeException("Failed to create the GLFW window");
-        
+
         final GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         glfwSetWindowPos(window, (vidmode.width() - WINDOW_WIDTH) / 2, (vidmode.height() - WINDOW_HEIGHT) / 2);
-        
+
         glfwMakeContextCurrent(window);
         GL.createCapabilities(true);
         GLAPI.setupDebugPrinting();
         glfwSwapInterval(1);
-        
+
         glfwSetKeyCallback(window, (window, key, scancode, action, mods) ->
         {
             if (action == GLFW_RELEASE)
@@ -197,12 +197,12 @@ public class Bootstrap
                 }
             }
         });
-        
+
         glfwSetFramebufferSizeCallback(window, this::onResize);
-        
+
         glfwShowWindow(window);
     }
-
+    
     private void onResize(long window, int width, int height)
     {
         if ((width != this.width || height != this.height)
@@ -210,24 +210,24 @@ public class Bootstrap
         {
             this.width = width;
             this.height = height;
-            
+
             if (mainFrameBufferTex != null)
                 mainFrameBufferTex.dispose();
             mainFrameBufferTex = null;
-            
+
             cameraTransform = null;
             resetRender();
         }
     }
-
+    
     private void resetRender()
     {
         numPasses = 0;
         totalFrameTime = 0;
-        
+
         setupScene(sceneTime);
     }
-    
+
     private void loop()
     {
         prevLoopTime = glfwGetTime();
@@ -236,7 +236,7 @@ public class Bootstrap
             final double delta = glfwGetTime() - prevLoopTime;
             timeElapsed += delta;
             prevLoopTime = glfwGetTime();
-            
+
             fpsCounter++;
             if (glfwGetTime() - lastFPSTime >= 1)
             {
@@ -244,33 +244,33 @@ public class Bootstrap
                 fpsCounter = 0;
                 lastFPSTime = glfwGetTime();
             }
-            
+
             if (timeElapsed % 1 < 0.05)
             {
                 System.out.println(fps);
             }
-            
+
             /*if (numPasses == PASSES_PER_FRAME)
             {
                 sceneTime += 1 / 30F;
                 //sceneTime += glfwGetTime() - lastFrameTime;
                 lastFrameTime = glfwGetTime();
-            
+
                 resetRender();
             }*/
-            
+
             render();
-            
+
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
     }
-    
+
     private void setupScene(double time)
     {
         updateCameraTransform(time);
     }
-    
+
     private void updateCameraTransform(double time)
     {
         final float angle = 20 * (float)Math.sin(Math.toRadians(time * 100));
@@ -283,7 +283,7 @@ public class Bootstrap
                 Quaternion.IDENTITY,
                 Vector3.ONE));*/
     }
-    
+
     private void setCameraTransform(Transform transform)
     {
         if (cameraTransform == null || !cameraTransform.equals(transform))
@@ -292,13 +292,13 @@ public class Bootstrap
             updateCamera();
         }
     }
-
+    
     private void updateCamera()
     {
-        projectionMatrix = Matrix4.createPerspectiveProjection(0.1F, 1000, 90, (float)width / height);
+        projectionMatrix = Matrix4.createPerspectiveProjection(0.1F, 1000, 110, (float)width / height);
         viewMatrix = cameraTransform.getRot().toMatrix4();
         final Matrix4 invViewProjMatrix = projectionMatrix.multiply(viewMatrix).invert();
-        
+
         cam00 = Vector4.create(-1, -1, 0, 1).multiply(invViewProjMatrix);
         cam00 = cam00.divide(cam00.getW());
         cam10 = Vector4.create(1, -1, 0, 1).multiply(invViewProjMatrix);
@@ -308,7 +308,7 @@ public class Bootstrap
         cam11 = Vector4.create(1, 1, 0, 1).multiply(invViewProjMatrix);
         cam11 = cam11.divide(cam11.getW());
     }
-
+    
     private void render()
     {
         if (mainFrameBufferTex == null)
@@ -317,14 +317,14 @@ public class Bootstrap
                     .format(Format.RGBA32F)
                     .build();
         }
-        
+
         if (numPasses < PASSES_PER_FRAME)
         {
             qSetup.begin();
-            
+
             mainFrameBufferTex.bindImage(0, Access.READ_WRITE, Format.RGBA32F);
             textureManager.getTexture("environment/table_mountain_2_1k.exr").bind(0);
-            
+
             final ShaderProgram program1 = shaderManager.getProgram("compute_draw");
             program1.setUniform("framebuffer", 0);
             program1.setUniform("numPasses", numPasses + 1);
@@ -338,41 +338,41 @@ public class Bootstrap
             program1.setUniform("environmentTex", 0);
             //program1.setUniform("stratifiedGridIndices", generateStratifiedGridIndices());
             program1.use();
-            
+
             final int numGroupsX = FastMath.fastCeil((float)mainFrameBufferTex.getWidth() / program1.getWorkgroupSizeX());
             final int numGroupsY = FastMath.fastCeil((float)mainFrameBufferTex.getHeight() / program1.getWorkgroupSizeY());
-            
+
             qSetup.end();
-            
+
             qCompute.begin();
             program1.dispatchCompute(numGroupsX, numGroupsY, 1);
             qCompute.end();
-            
+
             qBarrier.begin();
             GLAPI.memoryBarrier(GL42.GL_TEXTURE_FETCH_BARRIER_BIT | GL42.GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
             qBarrier.end();
         }
-
+        
         if (numPasses < PASSES_PER_FRAME)
         {
             qComposite.begin();
-            
+
             FrameBuffer.unBind();
             GLAPI.setViewport(0, 0, width, height);
-            
+
             mainFrameBufferTex.bind(0);
-            
+
             final ShaderProgram program2 = shaderManager.getProgram("post_composite");
             program2.setUniform("tex", 0);
             program2.setUniform("exposure", 1F);
             //program2.setUniform("exposure", (FastMath.sin((float)timeElapsed * 2) * 0.5F + 0.5F) * 100);
             program2.use();
-            
+
             FullscreenQuadRenderer.render();
-            
+
             qComposite.end();
         }
-        
+
         if (numPasses < PASSES_PER_FRAME)
         {
             final long tSetup = qSetup.getResult();
@@ -387,24 +387,24 @@ public class Bootstrap
                     + "\n    Barrier:\t" + tBarrier / 1e6 + " ms"
                     + "\n  Composite:\t" + tComposite / 1e6 + " ms");
         }
-
+        
         if (numPasses < PASSES_PER_FRAME)
             numPasses++;
     }
-    
+
     /*private static final int MAX_STRATIFIED_DIMENSIONS = 5;
     private static final int SAMPLES_PER_PIXEL = 100;
     private final int[] stratifiedGridIndices = new int[MAX_STRATIFIED_DIMENSIONS * SAMPLES_PER_PIXEL];
-    
+
     private int[] generateStratifiedGridIndices()
     {
         for (int i = 0; i < MAX_STRATIFIED_DIMENSIONS; i++)
         {
             final int[] indices = new int[SAMPLES_PER_PIXEL];
-    
+
             for (int j = 0; j < SAMPLES_PER_PIXEL; j++)
                 indices[j] = j;
-
+    
             for (int k = SAMPLES_PER_PIXEL; k > 1; k--)
             {
                 final int rand = RAND.nextInt(k);
@@ -412,10 +412,10 @@ public class Bootstrap
                 indices[rand] = indices[k - 1];
                 indices[k - 1] = tmp;
             }
-    
+
             System.arraycopy(indices, 0, stratifiedGridIndices, i * SAMPLES_PER_PIXEL, SAMPLES_PER_PIXEL);
         }
-    
+
         return stratifiedGridIndices;
     }*/
 }
